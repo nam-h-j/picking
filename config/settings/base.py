54aa70b7a9b7673967d6259d6a813e1927ba7c48
nam <hzzn74@gmail.com>
2019-11-08 15:25:43 +0900

    model creation

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,6 +76,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "picking.users.apps.UsersConfig",
+    "picking.images.apps.ImagesConfig",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
