5c26127064c625dd0b330c54a004824473765dea
nam <hzzn74@gmail.com>
2019-12-23 14:49:05 +0900

    like API Done

--- picking/images/views.py
+++ picking/images/views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from . import models, serializers
 
 class Feed(APIView) :
-    def get(self, request, format=None):
+    def get(self, request, format=None) :
         following_users = request.user.following.all()
         image_list = []
 
@@ -18,3 +19,26 @@ class Feed(APIView) :
         serializer = serializers.ImageSerializer(sorted_list, many=True)
 
         return Response(serializer.data)
+
+class LikeImage(APIView) :
+    def get(self, request, id, format=None) :
+
+        try :
+            get_image = models.Image.objects.get(id=id)
+        except models.Image.DoesNotExist :
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try :
+            check_like = models.Like.objects.get(
+                creator = request.user,
+                image = get_image
+            )
+            check_like.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except models.Like.DoesNotExist :
+            new_like = models.Like.objects.create(
+                creator = request.user,
+                image = get_image
+            )
+            new_like.save()
+            return Response(status=status.HTTP_201_CREATED)
